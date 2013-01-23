20409df05d1a2896eab182bdfaae5baa24405260
Patric Pettersson <ppe@KAP-W010.kaplans.se>
2013-01-23 10:26:00 +0100

    Basic functionality

---
+++ WT/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var worker = new Worker();
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                switch (args[i])
+                {
+                    case "-h":
+                        i++;
+                        worker.WorkHours = Convert.ToInt32(args[i]);
+                        break;
+                    case "-m":
+                        i++;
+                        worker.WorkMinutes = Convert.ToInt32(args[i]);
+                        break;
+                }
+            }
+
+            if (worker.WorkHours > 0 || worker.WorkMinutes > 0)
+                worker.Start();
+        }
+    }
+}
---
+++ WT/Worker.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+
+namespace WT
+{
+    class Worker
+    {
+        private Timer _timer;
+        private static bool _timerPaused;
+
+        private static DateTime _endTime;
+
+        public int WorkHours { get; set; }
+        public int WorkMinutes { get; set; }
+
+        public void Start()
+        {
+            _endTime = new DateTime();
+            _endTime = DateTime.Now.AddHours(WorkHours).AddMinutes(WorkMinutes);
+            _timerPaused = false;
+            _timer = new Timer();
+            _timer.Elapsed += new ElapsedEventHandler(TimerTick);
+            _timer.Interval = 1000;
+            _timer.Enabled = true;
+
+            TheLoop();
+
+            _timer.Enabled = false;
+        }
+
+        private void TheLoop()
+        {
+            string key = string.Empty;
+
+            while (key.ToLower() != "q")
+            {
+                key = Console.ReadKey().KeyChar.ToString();
+
+                switch (key.ToLower())
+                {
+                    case "p": //pause resume
+                        _timerPaused = _timerPaused == true ? false : true;
+                        break;
+                    case "r":
+                        _endTime = DateTime.Now.AddHours(WorkHours).AddMinutes(WorkMinutes);
+                        break;
+                }
+            }
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            if (_timerPaused)
+            {
+                _endTime = _endTime.AddSeconds(1);
+                
+            }
+
+            var timeLeft = _endTime - DateTime.Now;
+            Console.Clear();
+            Console.WriteLine("Dagens slut: " + _endTime.ToString());
+            Console.WriteLine("Tid kvar: " + timeLeft.Hours + "h " + timeLeft.Minutes + "m " + timeLeft.Seconds + "s");
+        }
+    }
+}
